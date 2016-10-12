9dbfbd6b808597d372d014724f17c370525b972d
Ben Gaines <bengaines92@gmail.com>
2016-10-12 13:11:21 -0500

    Now with custom front-page template

--- wp-content/themes/twentysixteen-child/functions.php
+++ wp-content/themes/twentysixteen-child/functions.php
@@ -1,14 +1,9 @@
 <?php
-function my_theme_enqueue_styles() {
 
-    $parent_style = 'parent-style'; // This is 'twentysixteen-style' for the Twenty Sixteen theme.
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
+function enqueue_parent_styles() {
+		wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css');    
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
 ?>
